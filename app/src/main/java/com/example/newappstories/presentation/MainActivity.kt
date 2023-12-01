02cf08cd0acc66c3066051eb120f527d4079b9b8
Rustam <gadelshin2011@gmail.com>
2023-12-01 09:30:17 +0500

    Initial commit

---
+++ app/src/main/java/com/example/newappstories/presentation/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.newappstories.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.example.newappstories.R
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navFragment) as NavHostFragment
+        navController = navHostFragment.navController
+    }
+}
